22caaccd2af2bd1bad16017e27f73345be4b34a3
Sayf <sayfgmaul@gmail.com>
2021-10-21 20:15:56 +0200

    some more fixes

--- dataviz.py
+++ dataviz.py
@@ -6,11 +6,12 @@ results = []
 for line in open("results.txt", "r"):
     results.append(int(line))
 
+results = np.array(results, dtype=int)
 # print results
 fig1, ax1 = plt.subplots()
 ax1.set_title('Steps it takes to solve Tower of Hanoi')
 ax1.boxplot(results)
 plt.savefig('boxplot.png')
 
-results = np.array(results, dtype=int)
 print 'average steps it takes to complete hanoi', round(results.mean())
+print 'Standard Deviation = ', round(results.std())
